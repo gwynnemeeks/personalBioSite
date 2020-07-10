58591ddee94122854dddb299705135adc3e69fc9
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-07-09 21:08:39 -0500

    wip projects print to the dom with light styling

--- src/javascripts/components/projects/projects.js
+++ src/javascripts/components/projects/projects.js
@@ -1,22 +1,26 @@
 import projectData from '../../helpers/data/projectData';
 import utils from '../../helpers/utils';
 
+import './projects.scss';
+
 const projectsCollection = projectData.getProjects();
 
 const createProjectCards = () => {
   let domString = '';
   projectsCollection.forEach((projects) => {
-    domString += `<div class="projects">
-      <h2>${projects.title}</h2>
-      <img src="${projects.screenshot}" alt="">
-      <h3>${projects.description}</h3>
-      <p>${projects.technologiesUsed}</p>
-      <h4>${projects.url}</h4>
-      <h4>${projects.githubUrl}</h4>
-      </div>
-      `;
+    domString += `
+    <div class="card" style="width: 18rem;" id="cssCard">
+    <div class="card-body">
+    <h5 class="card-title">${projects.title}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">${projects.technologiesUsed}</h6>
+    <p class="card-text">${projects.description}</p>
+    <a href="#${projects.url}" class="card-link">${projects.title}</a>
+    <a href="#${projects.githubUrl}" class="card-link">gitHub</a>
+  </div>
+</div>
+    `;
   });
-  utils.printToDom('#projectsCards', domString);
+  utils.printToDom('#projectCards', domString);
 };
 
 export default { createProjectCards };
--- src/javascripts/helpers/data/projectData.js
+++ src/javascripts/helpers/data/projectData.js
@@ -1,40 +1,40 @@
 const projects = [
   {
-    title: 'Cool Project',
+    title: 'Narnia ChatLog',
     screenshot: 'http://via.placeholder.com/100',
-    description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
+    description: 'A simple chatlog for the Pensieves and Aslan', // A good project description includes 'the what', 'the why', and 'the how'.
     technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
     available: true,
-    url: 'https://github.com/nss-evening-cohort-8/js-part-deux', // Github links
-    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+    url: 'https://github.com/gwynnemeeks/chatty-delta-narnia', // Github links
+    githubUrl: 'https://github.com/gwynnemeeks/chatty-delta-narnia',
   },
   {
-    title: 'Cool Project',
+    title: 'Gwynterist',
     screenshot: 'http://via.placeholder.com/100',
-    description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
-    available: false,
-    url: 'https://github.com/nss-evening-cohort-8/js-part-deux', // Github links
-    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
-  },
-  {
-    title: 'Cool Project',
-    screenshot: 'http://via.placeholder.com/100',
-    description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
+    description: 'An attempt to make a simplified Pinterest', // A good project description includes 'the what', 'the why', and 'the how'.
     technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
     available: true,
-    url: 'https://github.com/nss-evening-cohort-8/js-part-deux', // Github links
-    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+    url: 'https://github.com/gwynnemeeks/pinterest', // Github links
+    githubUrl: 'https://github.com/gwynnemeeks/pinterest',
   },
-  {
-    title: 'Cool Project',
-    screenshot: 'http://via.placeholder.com/100',
-    description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
-    available: true,
-    url: 'https://github.com/nss-evening-cohort-8/js-part-deux', // Github links
-    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
-  },
+  // {
+  //   title: 'Cool Project',
+  //   screenshot: 'http://via.placeholder.com/100',
+  //   description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
+  //   technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
+  //   available: true,
+  //   url: 'https://github.com/nss-evening-cohort-8/js-part-deux', // Github links
+  //   githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+  // },
+  // {
+  //   title: 'Cool Project',
+  //   screenshot: 'http://via.placeholder.com/100',
+  //   description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
+  //   technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
+  //   available: true,
+  //   url: 'https://github.com/nss-evening-cohort-8/js-part-deux', // Github links
+  //   githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+  // },
 ];
 
 const getProjects = () => projects;
