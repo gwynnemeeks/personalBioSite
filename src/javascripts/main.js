c5b38c69bd07094143d07b3b630abb80ebc7ec22
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-08-06 15:17:37 -0500

    technically close to being a single page app

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,10 +1,10 @@
-// import '../styles/main.scss';
-// import navBar from './components/navBar/navbar';
-// import projectCards from '../javascripts/components/projects/projects'
+import '../styles/main.scss';
+import navBar from './components/navBar/navbar';
+import projectCards from './components/projects/projects';
 
-// const init = () => {
-//   navBar.buildNavBar();
-//   projectCards.createProjectCards();
-// };
+const init = () => {
+  navBar.buildNavBar();
+  projectCards.createProjectCards();
+};
 
-// init();
+init();
