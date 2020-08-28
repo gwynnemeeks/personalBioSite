eaccc63d63e2f9ef77376fc8b66a9eec10c974b6
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-08-28 16:36:42 -0500

    wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,12 @@
+// import firebase from 'firebase';
+// import apiKeys from './helpers/data/apiKeys.json';
+
 import '../styles/main.scss';
 import navBar from './components/navBar/navbar';
 import clickEvents from './components/clickEvents/clickEvents';
 
 const init = () => {
+  // firebase.initializeApp(apiKeys.firebaseConfig);
   navBar.buildNavBar();
   clickEvents.clickEvents();
 };
