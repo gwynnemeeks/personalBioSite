f7ccffd131ba830637f48ce4f0589aa78a38fa2c
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-12-06 16:11:17 -0600

    wip

--- src/javascripts/components/contact/contact.js
+++ src/javascripts/components/contact/contact.js
@@ -5,9 +5,9 @@ import './contact.scss';
 const buildContactInfo = () => {
   const domString = `
     <h1>Gwynne Meeks</h1>
-    <a href="mailto:meeks.gwynne@gmail.com"><i class="far fa-envelope fa-2x"></i></a>
-    <a href="https://github.com/gwynnemeeks" target="_new"><i class="fab fa-github fa-2x"></i></a>
-    <a href="https://www.linkedin.com/in/gwynnemeeks/" target="_new"><i class="fab fa-linkedin fa-2x"></i></a>
+    <a href="mailto:meeks.gwynne@gmail.com"><i class="far fa-envelope fa-3x"></i></a>
+    <a href="https://github.com/gwynnemeeks" target="_new"><i class="fab fa-github fa-3x"></i></a>
+    <a href="https://www.linkedin.com/in/gwynnemeeks/" target="_new"><i class="fab fa-linkedin fa-3x"></i></a>
     
     `;
   utils.printToDom('#contactInfo', domString);
