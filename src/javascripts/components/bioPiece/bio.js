6165e74262887890a800e5b5294f978f37122d22
Gwynne Meeks <meeks.gwynne@gmail.com>
2021-04-23 11:14:49 -0500

    wip bio

--- src/javascripts/components/bioPiece/bio.js
+++ src/javascripts/components/bioPiece/bio.js
@@ -14,11 +14,17 @@ const buildBio = () => {
   <div class="card-body">
   <h5 class="card-title">Writer. Martial Artist. Developer.</h5>
   <p class="card-text">Hello World! I'm a recent transplant to Nashville, TN from
-  San Diego, CA. I attended Nashville Software School to become a Full 
+  San Diego, CA. I attended Nashville Software School during 2020-2021 to become a Full 
   Stack Software Developer after being introduced to coding by a family member.
   I love the creativity it encourages in problem solving. The more I learn about coding 
   and the influence it has in our world, the more I wanted to become a 
-  part of that conversation.</p>
+  part of that conversation.
+  <br>
+  I studied Choi Li Fut Kung Fu for seven years and bring that dedication and passion
+  into all areas of my personal and professional life. Like my time at NSS, it has taught
+  me how to approach large problems that require an open mind to resolve the problems
+  in front of me.
+  </p>
   </div>
   </div>
   </div>
